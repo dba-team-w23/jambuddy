a8eb55f64acf7b18a6800fa043dad1e0b8626650
dkeech <ddkeech@gmail.com>
2023-01-25 12:59:48 -0500

    dummy posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import Body from './components/Body';
 import Navigation from './components/Navigation';
 import { purple } from '@mui/material/colors';
--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,10 +1,32 @@
+import axios from "axios";
 import React from "react";
 
-function Posts(){   
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
+
+export default function Posts(){   
+    const [posts, setPosts] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setPosts(response.data);
+        });
+    }, []);
+    console.log(posts.map((post) => (
+        <div>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        </div>
+    )));
+
     return(
         <div>
-            <h1>Posts</h1>
+            {
+                posts.map((post) => (
+                    <>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    </>
+            ))}
         </div>
     )
 }
-export default Posts;
