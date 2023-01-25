3562122792b63ef65ab4bcaa07fbc6223631c764
dkeech <ddkeech@gmail.com>
2023-01-25 13:10:21 -0500

    user dummy data

--- frontend/src/components/Profiles.js
+++ frontend/src/components/Profiles.js
@@ -1,10 +1,33 @@
 import React from "react";  
+import axios from "axios";
 
-function Profiles(){
+const baseURL = "https://api.github.com/users";
+
+export default function Profiles(){
+    const [profiles, setProfiles] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setProfiles(response.data);
+        });
+    }, []);
+    console.log(profiles.map((profile) => (
+        <div>
+            <h2>{profile.login}</h2>
+            <p>{profile.avatar_url}</p>
+        </div>
+    )));
+
     return(
         <div>
-            <h1>Profiles</h1>
-        </div>
-    )
+        {profiles.map((profile) => (
+            <>
+            <h2>{profile.login}</h2>
+            <img src={profile.avatar_url} />
+            </>
+        )
+    )}
+    </div>
+    )   
+
 }
-export default Profiles;
