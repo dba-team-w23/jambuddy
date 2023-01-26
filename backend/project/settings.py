1e56dbe370122577ed28a9193b886a9ac40715ef
asa-leholland <asaleholland@gmail.com>
2023-01-26 02:34:01 -0800

    Revert "WIP: attempt at adding vercel backend"
    This reverts commit 0bea2c415decf77c65680f298a102542c2d6cf8d.

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': 5432,
+        'PORT': '',
     }
 }
 
