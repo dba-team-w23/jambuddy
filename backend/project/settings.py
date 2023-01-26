f9a55db1e74ad8622c411055f998aaceafe94ce7
asa-leholland <asaleholland@gmail.com>
2023-01-26 01:51:57 -0800

    WIP: add connection to DB

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -27,13 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost']  # this is the host that Docker uses to run application
 INSTALLED_APPS = [
-    'corsheaders'  # this will be installed later
-]
-
-
-# Application definition
-
-INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,6 +53,18 @@ MIDDLEWARE = [
 
 # Used by Django Debug Toolbar
 INTERNAL_IPS = ['127.0.0.1']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scwxgusz',
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': '',
+    }
+}
 
 
 ROOT_URLCONF = 'project.urls'
@@ -87,16 +93,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
-}
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 
