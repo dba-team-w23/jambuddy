9403c14c9b925655a1bdd8063d4004768a3a3187
dkeech <ddkeech@gmail.com>
2023-01-27 17:16:44 -0500

    better file upload

--- frontend/src/components/partials/FileUpload.js
+++ frontend/src/components/partials/FileUpload.js
@@ -1,50 +1,20 @@
-import { FormControl, InputLabel, Input, FormHelperText, Button } from '@mui/material';
-import {Image} from '@mui/icons-material';
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
 
-export default function FileUpload() {
-  const [image, setImage] = React.useState(null);
-  const [error, setError] = React.useState(null);
-
-  const handleChange = (event) => {
-    if (event.target.files.length > 0) {
-      setImage(event.target.files[0]);
-      setError(null);
-    } else {
-      setError('Please select an image');
-    }
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // perform upload logic here
-    // e.g. using fetch or axios
-    console.log('submitting image', image);
-    //Resetting the form
-    setImage(null);
-  };
-
+export default function UploadButtons() {
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl>
-        <InputLabel htmlFor="image-upload">Upload Image</InputLabel>
-        <Input
-          id="image-upload"
-          type="file"
-          onChange={handleChange}
-          endAdornment={<Image />}
-        />
-        {error && <FormHelperText error>{error}</FormHelperText>}
-      </FormControl>
-      <br/><br/>
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        disabled={!image}
-      >
-        Submit
+    <Stack direction="row" alignItems="center" spacing={2}>
+      <Button variant="contained" component="label">
+        Upload
+        <input hidden accept="image/*" multiple type="file" />
       </Button>
-    </form>
+      <IconButton color="primary" aria-label="upload picture" component="label">
+        <input hidden accept="image/*" type="file" />
+        <PhotoCamera />
+      </IconButton>
+    </Stack>
   );
 }
