5492c2df81663ae3c78d2b4d1a0dad8e03530278
asa-leholland <asaleholland@gmail.com>
2023-01-28 15:01:59 -0800

    FEAT: add Instrument table to admin

--- backend/jamrequestmodule/admin.py
+++ backend/jamrequestmodule/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Instrument
+
+
+class InstrumentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type')
+
+
+admin.site.register(Instrument, InstrumentAdmin)
--- backend/jamrequestmodule/models.py
+++ backend/jamrequestmodule/models.py
@@ -7,3 +7,6 @@ class Instrument(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.name)
