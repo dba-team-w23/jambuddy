99419f8927eede84b7cf34fbabaf93753abbb609
asa-leholland <asaleholland@gmail.com>
2023-01-29 13:09:05 -0800

    FEAT: add local option to settings db

--- backend/settings.py
+++ backend/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'jamrequestmodule.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# For ElephantSQL (PostgreSQL) Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,6 +95,13 @@ DATABASES = {
     }
 }
 
+# For Local Development (Not tied to ElephantSQL)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
