a63146f37b49504abf98f71d7056cc8c0cdadbda
asa-leholland <asaleholland@gmail.com>
2023-01-29 13:08:46 -0800

    FEAT: seed data scripts

---
+++ backend/jamrequestmodule/fixtures/seed_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO jamrequestmodule_users (fname, lname, username, password, lastlogin, created, street, city, state, zipcode, phone, email)
+VALUES ('John', 'Smith', 'jsmith', '123', '2023-01-28 21:00:00.000 -0800', '2023-01-28 21:00:00.000 -0800','123 Main St', 'Anytown', 'CA', '12345', '555-555-5555', 'john.smith@example.com'),
+       ('Jane', 'Doe', 'jdoe', '123', '2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800', '456 Park Ave', 'Anytown', 'CA', '67890', '555-555-5555', 'jane.doe@example.com'),
+       ('Bob', 'Johnson', 'bjonny', '123', '2023-01-28 21:00:00.000 -0800', '2023-01-28 21:00:00.000 -0800','789 Elm St', 'Anytown', 'NY', '09876', '555-555-5555', 'bob.johnson@example.com'),
+       ('Samantha', 'Williams', 'swilly', '123', '2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800','321 Oak St', 'Anytown', 'FL', '54321', '555-555-5555', 'samantha.williams@example.com'),
+       ('Michael', 'Brown', 'mbrown', '123','2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800', '654 Pine St', 'Anytown', 'TX', '67890', '555-555-5555', 'michael.brown@example.com'),
+       ('Ashley', 'Jones', 'ajones', '123','2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800', '987 Cedar St', 'Anytown', 'IL', '13579', '555-555-5555', 'ashley.jones@example.com'),
+       ('Joshua', 'Miller', 'jmiller', '123','2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800', '246 Birch St', 'Anytown', 'PA', '24680', '555-555-5555', 'joshua.miller@example.com'),
+       ('Jessica', 'Davis', 'jdavis', '123','2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800', '369 Maple St', 'Anytown', 'OH', '36912', '555-555-5555', 'jessica.davis@example.com'),
+       ('Jacob', 'Garcia', 'jgarcia', '123','2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800', '159 Willow St', 'Anytown', 'NC', '15935', '555-555-5555', 'jacob.garcia@example.com'),
+       ('Amanda', 'Rodriguez', 'aroddd', '123','2023-01-28 21:00:00.000 -0800','2023-01-28 21:00:00.000 -0800', '753 Oak St', 'Anytown', 'WA', '753', '555-555-5555', 'amanda.rodriguez@example.com');
