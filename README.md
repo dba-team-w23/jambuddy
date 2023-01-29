654e5e7e54591515539178a5049cd1ff4ea17c48
asa-leholland <asaleholland@gmail.com>
2023-01-29 10:43:58 -0800

    FEAT: add production update script

--- README.md
+++ README.md
@@ -23,13 +23,9 @@ To build (or rebuild) the Docker container, execute `bash rebuild-docker.sh` in 
 
 1. Log in to <https://www.pythonanywhere.com/user/dbajamteam/consoles/>
 2. Open a console, cd into jambuddy/backend
-3. run `git pull`
-4. run `workon myvirtualenv`
-5. run `pip install -r requirements.txt`
-6. run `python manage.py makemigrations`
-7. run `python manage.py migrate`
-8. Navigate to <https://www.pythonanywhere.com/user/dbajamteam/webapps/#tab_id_dbajamteam_pythonanywhere_com>
-9. Click Reload (green button)
+3. run `bash prod-update.sh`
+4. Navigate to <https://www.pythonanywhere.com/user/dbajamteam/webapps/#tab_id_dbajamteam_pythonanywhere_com>
+5. Click Reload (green button)
 
 ## Backend Test Suite
 
---
+++ backend/prod-update.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+
+check_backend_env_file(){
+    if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+        if [ -f $PWD/backend/.env ]; then
+            echo ".env file found in $(pwd)"
+        else
+            echo "Error: .env file not found in $(pwd)"
+            exit 1
+        fi
+    else
+        if [ -f backend/.env ]; then
+            echo ".env file found in /backend"
+        else
+            echo "Error other: .env file not found in /backend"
+            exit 1
+        fi
+    fi
+}
+
+check_variables(){
+    for variable in "$@"
+    do
+        if grep -q "$variable" backend/.env; then
+            echo "$variable is defined in .env"
+        else
+            echo "Error: $variable is not defined in .env"
+            exit 1
+        fi
+    done
+}
+
+assert_backend_directory() {
+    if [ "$(basename $(pwd))" != "backend" ]; then
+        echo "Error: Must be run from the backend directory"
+        exit 1
+    fi
+}
+
+check_backend_env_file
+check_variables "POSTGRES_HOST" "POSTGRES_USER" "POSTGRES_PASSWORD" "SECRET_KEY" "DEBUG"
+assert_backend_directory
+git pull
+workon myvirtualenv
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
