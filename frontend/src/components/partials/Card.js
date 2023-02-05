737042e3739d3d99cd2049f961e74f556dfdcef3
Dan <ddkeech@gmail.com>
2023-02-05 13:39:15 -0500

    stop photos from refreshing

--- frontend/src/components/partials/Card.js
+++ frontend/src/components/partials/Card.js
@@ -28,9 +28,20 @@ const ExpandMore = styled((props) => {
 
 export default function ProfileCard({profile}) {
   const [expanded, setExpanded] = React.useState(false);
+  //temp photo
+  const [photoURL, setPhotoURL] = React.useState(profile.photo || `https://source.unsplash.com/random/?face&${Math.random()}`);
+  // temporary bios
   const shortbio = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
   const longbio = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
-  
+
+  React.useEffect(() => {
+    if(!profile.photo){
+      console.log("no photo")
+      return;
+    }
+    setPhotoURL(profile.photo);
+    
+  }, [profile])
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -55,7 +66,7 @@ export default function ProfileCard({profile}) {
       <CardMedia
         component="img"
         height="194"
-        image={profile.photo ? profile.photo : `https://source.unsplash.com/random/?face&${Math.random()}`}
+        image={photoURL}
         alt={profile.fname}
       />
       <CardContent>
