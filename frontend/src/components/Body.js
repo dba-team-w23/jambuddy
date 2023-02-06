56b1887cc70d537e1c9fe9e13bbf5e8a64bdd174
asa-leholland <asaleholland@gmail.com>
2023-02-05 19:20:04 -0800

    WIP: jam request form

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Feed from './Feed';
 import Profile from './Profile';
 import JamRequests from './JamRequests';
+import NewJamRequest from './NewJamRequest';
 import Profiles from './Profiles';
 import Search from './Search';
 import SignIn from './SignIn';
@@ -16,7 +17,7 @@ export default function Body(props) {
     const [signedIn, setSignedIn] = React.useState(true);
     // const [username, setUsername] = React.useState(props.userId)
     const [userId, setUserId] = React.useState(5)
-   
+
     const handleChange = () => {
       setSignedIn(!signedIn);
     };
@@ -26,7 +27,7 @@ export default function Body(props) {
         <Navbar signedIn={signedIn} setSignedIn={setSignedIn}/>
         <div className="signedIn">
         {signedIn ? <h2 onClick={handleChange}>Signed in as {userId}</h2> : <h2 onClick={handleChange}>Signed out</h2>}
-           
+
         <Routes>
             <Route path="/" element={signedIn ? <JamRequests /> : <SignIn />} />
             <Route path="signin" element={<SignIn />} />
@@ -34,6 +35,7 @@ export default function Body(props) {
             <Route path="search" element={<Search />} />
             <Route path="profiles" element= { signedIn ? <Profiles /> : "Sign in to see profiles"} />
             <Route path="jamrequests" element= { signedIn ? <JamRequests /> : "Sign in to see Jam Requests"} />
+            <Route path="newjamrequest" element= { signedIn ? <NewJamRequest /> : "Sign in to see Jam Requests"} />
             <Route path="profile" element= { signedIn ? <Profile signedIn={signedIn} userId={userId} /> : "Sign in to create a profile"}/>
             <Route path="feed" element= { <Feed />} />
             <Route path="logout" onClick={handleChange} element={<SignIn />} />
---
+++ frontend/src/components/NewJamRequest.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const BASE_URL = "https://dbajamteam.pythonanywhere.com/api/";
+
+const JamRequestForm = () => {
+  const [instruments, setInstruments] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [experienceLevels, setExperienceLevels] = useState([]);
+  const [selectedInstruments, setSelectedInstruments] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [selectedExperienceLevel, setSelectedExperienceLevel] = useState("");
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [instrumentsResponse, genresResponse, experienceLevelsResponse] = await Promise.all([
+          axios.get(BASE_URL + "instruments/"),
+          axios.get(BASE_URL + "musicgenres/"),
+          axios.get(BASE_URL + "experiencelevels/"),
+        ]);
+
+        setInstruments(instrumentsResponse.data);
+        setGenres(genresResponse.data);
+        setExperienceLevels(experienceLevelsResponse.data);
+
+        console.log(instrumentsResponse.data);
+        console.log(genresResponse.data);
+        console.log(experienceLevelsResponse.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post(BASE_URL + "jamrequests/", {
+        userid: 1,
+        instrumentid: selectedInstruments,
+        genreid: selectedGenres,
+        location,
+        exp_level: selectedExperienceLevel,
+      });
+
+      // Show success message
+    } catch (error) {
+      console.error(error);
+      // Show error message
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Instruments:
+        <select multiple value={selectedInstruments} onChange={(event) => setSelectedInstruments(Array.from(event.target.selectedOptions, (option) => option.value))}>
+          {instruments.map((instrument) => (
+            <option key={instrument.id} value={instrument.id}>
+              {instrument.name}
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <br />
+
+      <label>
+        Genres:
+        <select multiple value={selectedGenres} onChange={(event) => setSelectedGenres(Array.from(event.target.selectedOptions, (option) => option.value))}>
+          {genres.map((genre) => (
+            <option key={genre.id} value={genre.id}>
+              {genre.genre}
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <br />
+      <label>
+        Location:
+        <input type="text" onChange={(event) => setLocation(event.target.value)} />
+      </label>
+      <br />
+      <br />
+      <label>
+        Experience Level:
+        <select onChange={(event) => setExperienceLevel(event.target.value)}>
+          {experienceLevels.map((level) => (
+            <option key={level.id} value={level.id}>
+              {level.level}
+            </option>
+          ))}
+        </select>
+      </label>
+      <br />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default JamRequestForm;
