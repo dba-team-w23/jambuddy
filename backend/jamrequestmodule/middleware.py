76134d87a4a100e749c54c10bd614c5022a58596
asa-leholland <asaleholland@gmail.com>
2023-02-05 16:03:06 -0800

    FEAT: add access to testing server

--- backend/jamrequestmodule/middleware.py
+++ backend/jamrequestmodule/middleware.py
@@ -1,8 +1,13 @@
+from django.conf import settings
+
+
 class CorsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        response["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        origin = request.META.get("HTTP_ORIGIN", "")
+        if origin in settings.CORS_ORIGIN_WHITELIST:
+            response["Access-Control-Allow-Origin"] = origin
         return response
