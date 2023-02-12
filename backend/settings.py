c3999b8ea47a7c722210bd9b4d84ebb228a7faa9
asa-leholland <asaleholland@gmail.com>
2023-02-11 20:03:38 -0800

    FEAT: attempt at bypassing cors on BE

--- backend/settings.py
+++ backend/settings.py
@@ -175,8 +175,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = [
-     "http://jambuddy.vercel.app/",
-     "https://jambuddy.vercel.app/",
+     "http://jambuddy.vercel.app",
+     "https://jambuddy.vercel.app",
      'http://localhost:3000',
      'https://localhost:3000',
      'http://localhost:8000',
