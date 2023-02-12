635c1d9f9259a3583b29c1cd573ef53291a33c9f
asa-leholland <asaleholland@gmail.com>
2023-02-11 20:39:35 -0800

    FEAT: attempt at bypassing cors on BE

--- backend/settings.py
+++ backend/settings.py
@@ -53,14 +53,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.common.CommonMiddleware",
+    # "django.middleware.common.CommonMiddleware",
     # "jamrequestmodule.middleware.CorsMiddleware",
 
 ]
@@ -187,6 +189,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #     r'^(https?://)?(www\.)?(jambuddy\.vercel\.app|localhost:3000)\.*$'
 # )
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+     "http://jambuddy.vercel.app",
+     "https://jambuddy.vercel.app",
+     "http://jambuddy.vercel.app/",
+     "https://jambuddy.vercel.app/",
+     'http://localhost:3000',
+     'https://localhost:3000',
+     'http://localhost:8000',
+     'https://localhost:8000',
+]
+
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = True
 
