21864168453c0aa01abf51d007a352a2c5ef7103
Asa LeHolland <asaleholland@gmail.com>
2023-02-22 23:16:23 -0800

    Create seed_instruments.sql

---
+++ backend/jamrequestmodule/fixtures/seed_instruments.sql
@@ -0,0 +1,30 @@
+Begin;
+
+INSERT INTO jamrequestmodule_instrument (name, type) VALUES
+('Guitar', 'String'),
+('Piano', 'Keyboard'),
+('Drums', 'Percussion'),
+('Bass', 'String'),
+('Violin', 'String'),
+('Trumpet', 'Brass'),
+('Saxophone', 'Woodwind'),
+('Flute', 'Woodwind'),
+('Violin', 'String'),
+('Clarinet', 'Woodwind'),
+('Harmonica', 'Wind'),
+('Banjo', 'String'),
+('Mandolin', 'String'),
+('Harp', 'String'),
+('Accordion', 'Keyboard'),
+('Trombone', 'Brass'),
+('Viola', 'String'),
+('French horn', 'Brass'),
+('Oboe', 'Woodwind'),
+('Bagpipes', 'Wind'),
+('Ukulele', 'String'),
+('Sitar', 'String'),
+('Didgeridoo', 'Percussion'),
+('Tabla', 'Percussion');
+
+
+Commit;
