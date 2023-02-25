88eb4484a70ec098429ecab1d055602f50759c80
bretswalberg <swalberb@oregonstate.edu>
2023-02-25 14:40:29 -0700

    Removed junkie header crappola

--- backend/jamrequestmodule/views.py
+++ backend/jamrequestmodule/views.py
@@ -283,15 +283,9 @@ def login_user(request):
     if user is not None and user.is_authenticated:
         login(request, user)
         response = Response({"status":1, "profile_id":user.pk}, status=status.HTTP_200_OK)
-        response["Access-Control-Allow-Origin"]= "*"
-        response["Access-Control-Allow-Credentials"]="true"
-        response["Access-Control-Allow-Methods"]="GET,HEAD,OPTIONS,POST,PUT"
-        response["Access-Control-Allow-Headers"]="Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-
         return response
     else:
         response = Response({"status":0, "error":"Invalid username or password"}, status=status.HTTP_200_OK)
-        response["Access-Control-Allow-Headers"] = "content-type"
         return response
 
 
