2aa8e5438b7d653aed49c9859af0e07a42bab723
asa-leholland <asaleholland@gmail.com>
2023-03-10 20:57:08 -0800

    Revert "feat(Profile): Make images load as Centered Top vertical rather than centered centered"
    This reverts commit 75ec6485aefb819ce6814f423b92be90da3a6d8d.

--- frontend/src/components/partials/ProfileCard.js
+++ frontend/src/components/partials/ProfileCard.js
@@ -30,7 +30,6 @@ export default function ProfileCard({ profile }) {
             src={profile.photo ? profile.photo : "./sunset.jpg"}
             alt={profile.first_name}
             className="object-cover position-absolute w-full h-full"
-            style={{ objectPosition: "center top" }}
           />
         </div>
 
